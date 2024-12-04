54c682c619de4d3d5537c97b03d624e3abc17ad6
nehuenparrondo <parrondonehuen@gmail.com>
2024-12-04 18:52:01 -0300

    Create punto5.cpp

---
+++ punto5.cpp
@@ -0,0 +1,10 @@
+#include <iostream>  
+#include <string>  
+#include <vector>  
+using namespace std;  
+class Alumno {  
+private:  
+    string nombre;  
+    double nota1;  
+    double nota2;  
+    double nota
